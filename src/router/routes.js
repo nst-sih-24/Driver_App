58848caf0f4763f595358821c5c84fd4b7eb2c8c
Oashe02 <mehtaoashe2006@gmail.com>
2024-12-12 14:35:22 +0530

    routes1

--- src/router/routes.js
+++ src/router/routes.js
@@ -12,11 +12,7 @@ const routes = [
       { path: 'nav_conductor', component: () => import('src/oldfiles/NavBar_Conductor.vue') },
       { path: 'yuvraj', component: () => import('src/oldfiles/Yuvraj_Page.vue') },
       { path: 'history', component: () => import('src/oldfiles/HistoryPage.vue') },
-      { path: 'help', component: () => import('src/oldfiles/HelpPage.vue') },
-      { path: 'src', component: () => import('src/oldfiles/HELlPage.vue') },
-
-
-
+      { path: 'help', component: () => import('src/oldfiles/HelpPage.vue') }
 
     ]
   },
